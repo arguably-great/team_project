8188683d3a8f615245db220471f6d46f63788b44
Jose Cruz Mendoza <josecm99@hotmail.com>
2019-07-18 21:30:46 -0700

    [NotesFeature][Initialization and General Progress]
    -Added various resources to strings.xml
    
    -Added permissions, metadata, and changed LAUNCHER to be my ARActivity in AndroidManifest.xml
    
    -Increased minSDKVersion, set compileOptions to Java 8, and added various dependencies to app Gradle
    -Added repository and dependency in project Gradle
    
    -Created various packages to organize new Classes and Files
    --Made ARActivity Class that handles showcasing two ViewRenderables at the same time
    --Created NoteTitleAdapter for RecyclerView (for when I was using Fragments, one which had a RecyclerView)
    --Created two Fragment Classes (one for Note Titles, and another for Note Contents)
    --Created two Interfaces that allowed for me to communicate information across my Fragments (via what was their shared Activity)
    --Created Note Class to contain the attributes of any Note
    
    -Added various XML files into layout
    --activity_ar contains the ARSceneView
    --fragment_note_contents was what I was using to show Note contents. Now just has a TextView for testing purposes
    --fragment_note_title was what I was using to show Note titles in RecyclerView. Now just has a TextView for testing purposes
    --item_note was what I was using to display specific pieces of the Note

---
+++ app/src/main/java/com/example/fbuteamproject/activities/ARActivity.java
@@ -0,0 +1,461 @@
+package com.example.fbuteamproject.activities;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.fbuteamproject.R;
+import com.example.fbuteamproject.fragments.NoteContentFragment;
+import com.example.fbuteamproject.interfaces.PassNoteToActivityListener;
+import com.example.fbuteamproject.models.Note;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+
+import org.parceler.Parcels;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class ARActivity extends AppCompatActivity implements PassNoteToActivityListener {
+
+    //TODO - Bring this back if needed
+//    private FragmentManager fragmentManager;
+
+    private final String APP_TAG = "NoteActivity";
+    private static final int RC_PERMISSIONS = 0x123;
+    private boolean installRequested;
+
+
+    private ArSceneView arSceneView;
+    private GestureDetector gestureDetector;
+    private Snackbar loadingMessageSnackbar = null;
+
+    private ViewRenderable noteTitlesRenderable;
+    private ViewRenderable noteContentsRenderable;
+
+    // True once scene is loaded
+    private boolean hasFinishedLoading = false;
+
+    // True once the scene has been placed.
+    private boolean hasPlacedFragments = false;
+
+
+//    private ArrayList<Note> notesTest;
+//    private NoteTitleAdapter noteTitleAdapter;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO - Make sure to change this when finished
+        setContentView(R.layout.activity_ar);
+
+        arSceneView = findViewById(R.id.ar_scene_view);
+
+        //Build the ViewRenderables
+        CompletableFuture<ViewRenderable> noteTitleStage =
+                ViewRenderable.builder().setView(this, R.layout.fragment_note_title).build();
+
+        CompletableFuture<ViewRenderable> noteContentsStage =
+                ViewRenderable.builder().setView(this, R.layout.fragment_note_contents).build();
+
+        CompletableFuture.allOf(
+                    noteTitleStage,
+                    noteContentsStage)
+                .handle(
+                        (notUsed, throwable) -> {
+                            // When you build a Renderable, Sceneform loads its resources in the background while
+                            // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                            // before calling get().
+
+                            if (throwable != null) {
+                                Log.e(APP_TAG, "Throwable found to be non-null in handle method");
+                                return null;
+                            }
+
+                            try {
+                                noteTitlesRenderable = noteTitleStage.get();
+                                noteContentsRenderable = noteContentsStage.get();
+
+                                // Everything finished loading successfully.
+                                hasFinishedLoading = true;
+
+                            } catch (InterruptedException | ExecutionException ex) {
+                                Log.e(APP_TAG, "Unable to load Renderables (IN CATCH STATEMENT)");
+                            }
+
+                            return null;
+                        });
+
+        // Set up a tap gesture detector.
+        gestureDetector =
+                new GestureDetector(
+                        this,
+                        new GestureDetector.SimpleOnGestureListener() {
+                            @Override
+                            public boolean onSingleTapUp(MotionEvent e) {
+                                Log.d(APP_TAG, "Just released my finger after having tapped");
+                                onSingleTap(e);
+                                return true;
+                            }
+
+                            @Override
+                            public boolean onDown(MotionEvent e) {
+                                return true;
+                            }
+                        });
+
+        // Set a touch listener on the Scene to listen for taps.
+        arSceneView
+                .getScene()
+                .setOnTouchListener(
+                        (HitTestResult hitTestResult, MotionEvent event) -> {
+                            // If the solar system hasn't been placed yet, detect a tap and then check to see if
+                            // the tap occurred on an ARCore plane to place the solar system.
+                            if (!hasPlacedFragments) {
+                                Log.d(APP_TAG, "Wanting to place Fragments for first time");
+                                return gestureDetector.onTouchEvent(event);
+                            }
+
+                            // Otherwise return false so that the touch event can propagate to the scene.
+                            Log.d(APP_TAG, "The Fragments are already out there, so we wanna" +
+                                    " do stuff in the scene");
+                            return false;
+                        });
+
+
+        // Set an update listener on the Scene that will hide the loading message once a Plane is
+        // detected.
+        arSceneView
+                .getScene()
+                .addOnUpdateListener(
+                        frameTime -> {
+                            if (loadingMessageSnackbar == null) {
+                                return;
+                            }
+
+                            Frame frame = arSceneView.getArFrame();
+                            if (frame == null) {
+                                return;
+                            }
+
+                            if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+                                return;
+                            }
+
+                            for (Plane plane : frame.getUpdatedTrackables(Plane.class)) {
+                                if (plane.getTrackingState() == TrackingState.TRACKING) {
+                                    hideLoadingMessage();
+                                }
+                            }
+                        });
+
+        requestCameraPermission(this);
+
+
+        //TODO = Bring this back if needed
+//        fragmentManager = getSupportFragmentManager();
+//
+//        NoteTitleFragment initFragment = new NoteTitleFragment();
+//
+//        initFragment.setPassNoteListener(this);
+//
+//        fragmentManager
+//                .beginTransaction()
+//                .add(R.id.fragmentContainer, initFragment)
+//                .commit();
+
+
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (arSceneView == null) {
+            return;
+        }
+
+        if (arSceneView.getSession() == null) {
+            // If the session wasn't created yet, don't resume rendering.
+            // This can happen if ARCore needs to be updated or permissions are not granted yet.
+            try {
+                Session session = createArSession(this, installRequested);
+                if (session == null) {
+                    installRequested = hasCameraPermission(this);
+                    return;
+                } else {
+                    arSceneView.setupSession(session);
+                }
+            } catch (UnavailableException e) {
+                Log.e(APP_TAG, "Some stuff went down...\n" + e.getLocalizedMessage() );
+            }
+        }
+
+        try {
+            arSceneView.resume();
+        } catch (CameraNotAvailableException ex) {
+            Log.e(APP_TAG, "Unable to get camera...");
+            finish();
+            return;
+        }
+
+
+        if (arSceneView.getSession() != null) {
+            showLoadingMessage();
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (arSceneView != null) {
+            arSceneView.pause();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (arSceneView != null) {
+            arSceneView.destroy();
+        }
+    }
+
+
+
+    private void onSingleTap(MotionEvent tap) {
+        if (!hasFinishedLoading) {
+            // We can't do anything yet.
+            Log.d(APP_TAG, "Not done loading Scene yet");
+            return;
+        }
+
+        Log.d(APP_TAG, "In onSingleTap and have finished loading the Scene");
+
+        Frame frame = arSceneView.getArFrame();
+        if (frame != null) {
+            if (!hasPlacedFragments && tryPlaceFragments(tap, frame)) {
+                hasPlacedFragments = true;
+            }
+        }
+    }
+
+
+    private boolean tryPlaceFragments(MotionEvent tap, Frame frame) {
+        if (tap != null && frame.getCamera().getTrackingState() == TrackingState.TRACKING) {
+            for (HitResult hit : frame.hitTest(tap)) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    // Create the Anchor.
+                    Log.d(APP_TAG, "Creating anchor to try and place Fragments");
+                    Anchor anchor = hit.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arSceneView.getScene());
+                    Node fragments = createFragments();
+                    anchorNode.addChild(fragments);
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+
+    private Node createFragments() {
+
+        Log.d(APP_TAG, "In Fragment creation method");
+
+        Node base = new Node();
+
+        Node noteTitles = new Node();
+        noteTitles.setParent(base);
+        noteTitles.setLocalPosition(new Vector3(0.0f, 0.5f, 0.0f));
+
+        Node noteVisual = new Node();
+        noteVisual.setParent(noteTitles);
+        noteVisual.setRenderable(noteTitlesRenderable);
+        noteVisual.setLocalScale(new Vector3(0.5f, 0.5f, 0.5f));
+
+        Node noteContents = new Node();
+        noteContents.setParent(noteTitles);
+        noteContents.setRenderable(noteContentsRenderable);
+        noteContents.setLocalScale(new Vector3(0.5f, 0.5f, 0.5f));
+        noteContents.setLocalPosition(new Vector3(1.0f, 0.0f, 0.0f));
+
+
+        View noteTitleView = noteTitlesRenderable.getView();
+        View noteContentView = noteContentsRenderable.getView();
+
+//        RecyclerView rvTest = noteTitleView.findViewById(R.id.rvNoteTitles);
+//
+//        notesTest = new ArrayList<>();
+//
+//        noteTitleAdapter = new NoteTitleAdapter(notesTest);
+//
+//        rvTest.setAdapter(noteTitleAdapter);
+//
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this );
+//
+//        rvTest.setLayoutManager(layoutManager);
+//
+//        generateNotes();
+
+
+
+
+
+
+        noteTitleView.setOnClickListener(new View.OnClickListener() {
+            int count = 0;
+
+            @Override
+            public void onClick(View v) {
+                ((TextView) noteContentView.findViewById(R.id.tvContents)).setText(String.format(getResources().getString(R.string.fragment_click_num), count + 1));
+                count += 1;
+            }
+        });
+
+        return base;
+    }
+
+
+//    private void generateNotes() {
+//
+//        for(int i = 0; i < 20; i++){
+//            String noteTitle = "NOTE " + (i+1);
+//
+//            String noteContent = "INSIDE OF NOTE " + (i+1);
+//
+//            Note note = new Note(noteTitle, noteContent);
+//
+//            notesTest.add(note);
+//            noteTitleAdapter.notifyItemInserted(notesTest.size() - 1);
+//        }
+//
+//    }
+
+
+
+
+
+    private void showLoadingMessage() {
+        if (loadingMessageSnackbar != null && loadingMessageSnackbar.isShownOrQueued()) {
+            return;
+        }
+
+        loadingMessageSnackbar =
+                Snackbar.make(
+                        ARActivity.this.findViewById(android.R.id.content),
+                        R.string.plane_finding,
+                        Snackbar.LENGTH_INDEFINITE);
+        loadingMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+        loadingMessageSnackbar.show();
+    }
+
+    private void hideLoadingMessage() {
+        if (loadingMessageSnackbar == null) {
+            return;
+        }
+
+        loadingMessageSnackbar.dismiss();
+        loadingMessageSnackbar = null;
+    }
+
+
+    private Session createArSession(Activity activity, boolean installRequested)
+            throws UnavailableException {
+        Session session = null;
+        // if we have the camera permission, create the session
+        if (hasCameraPermission(activity)) {
+            switch (ArCoreApk.getInstance().requestInstall(activity, !installRequested)) {
+                case INSTALL_REQUESTED:
+                    return null;
+                case INSTALLED:
+                    break;
+            }
+            session = new Session(activity);
+            // IMPORTANT!!!  ArSceneView requires the `LATEST_CAMERA_IMAGE` non-blocking update mode.
+            Config config = new Config(session);
+            config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+            session.configure(config);
+        }
+        return session;
+    }
+
+    private boolean hasCameraPermission(Activity activity) {
+        return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission(Activity activity) {
+        ActivityCompat.requestPermissions(
+                activity, new String[] {Manifest.permission.CAMERA}, ARActivity.RC_PERMISSIONS);
+    }
+
+
+
+
+    //TODO - Bring this back if needed
+    @Override
+    public void returnSelectedNote(Note selectedNoted) {
+        Bundle args = new Bundle();
+        args.putParcelable("note", Parcels.wrap(selectedNoted) );
+        NoteContentFragment contentFragment = new NoteContentFragment();
+        contentFragment.setArguments(args);
+
+//        fragmentManager
+//                .beginTransaction()
+//                .replace(R.id.fragmentContainer, contentFragment)
+//                .addToBackStack(null)
+//                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(getFragmentManager().getBackStackEntryCount() == 0) {
+            super.onBackPressed();
+        }
+        else {
+            getFragmentManager().popBackStack();
+        }
+    }
+
+
+
+}
