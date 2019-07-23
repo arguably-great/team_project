be4611852ddbcbb2f500010b4bcd56ffbe674423
Jose Cruz Mendoza <josecm99@hotmail.com>
2019-07-23 15:37:53 -0700

    [OrganizationFeature] [Improving Notes]
    - Changed XML to have TextView and EditText only instead of entire screen (looks better)
    - Corrected the calls in VideosActivity to make sure they work and reference the right components

--- app/src/main/java/com/example/fbuteamproject/activities/VideosActivity.java
+++ app/src/main/java/com/example/fbuteamproject/activities/VideosActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -527,7 +528,7 @@ public class VideosActivity extends AppCompatActivity {
 
     private void changePlanetScreenText(View nameView, View contentView, Planet currPlanet){
         ((TextView) nameView.findViewById(R.id.tvTitle) ).setText(currPlanet.getPlanetName() );
-        ((TextView) contentView.findViewById(R.id.tvContents) ).setText(currPlanet.getPlanetNotes() );
+        ((EditText) contentView.findViewById(R.id.etContents) ).setText(currPlanet.getPlanetNotes() );
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
