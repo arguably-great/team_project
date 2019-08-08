d23f60f91f357bb0a90c03d2fb12ccfedf2a3965
sophiac15 <sophiaclark@college.harvard.edu>
2019-08-08 10:29:44 -0700

    - Optimized photo search by adding keywords to config - Got rid of magic numbers

--- app/src/main/java/com/example/fbuteamproject/utils/Config.java
+++ app/src/main/java/com/example/fbuteamproject/utils/Config.java
@@ -29,7 +29,7 @@ public class Config {
         private ArrayList<ViewRenderable> entityPhotos;
 
 
-        Entity(String entityName, String modelId, String videoURL, Vector3 entityScaleVector, Context context){
+        Entity(String entityName, String modelId, String videoURL, Vector3 entityScaleVector, Context context, String configType){
             this.entityName = entityName;
             this.modelId = modelId;
             this.videoURL = videoURL;
@@ -159,19 +159,19 @@ public class Config {
 
         planetConfig0.entities.add(new Entity("Earth", "CHAHIN_EARTH.sfb",
                 "file:///sdcard/Movies/earth.mp4",
-                new Vector3(0.5f, 0.5f, 0.5f), AppConfig.getContext()));
+                new Vector3(0.5f, 0.5f, 0.5f), AppConfig.getContext(), "planet"));
         planetConfig0.entities.add(new Entity("Venus", "Venus_1241.sfb",
                 "file:///sdcard/Movies/VENUS.mp4",
-                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext()));
+                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext(), "planet"));
         planetConfig0.entities.add(new Entity("Neptune", "Neptune.sfb",
                 "file:///sdcard/Movies/neptune.mp4",
-                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext()));
+                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext(), "planet"));
         planetConfig0.entities.add(new Entity("Saturn", "13906_Saturn_v1_l3.sfb",
                 "file:///sdcard/Movies/saturn.mp4",
-                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext()));
+                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext(), "planet"));
         planetConfig0.entities.add(new Entity("Jupiter", "model.sfb",
                 "file:///sdcard/Movies/jupiter.mp4",
-                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext()));
+                new Vector3(0.2f, 0.2f, 0.2f), AppConfig.getContext(), "planet"));
 
         /*planetConfig1 = new AppConfig();
         planetConfig1.entities.add(new Entity("Venus", "5ovHBezIGyZ",
@@ -184,12 +184,12 @@ public class Config {
 
         animalConfig = new AppConfig();
 
-        animalConfig.entities.add(new Entity("Lion", "3XAJojWxSWz","https://pmdvod.nationalgeographic.com/NG_Video/493/847/1087143491881_1509643865712_1087150659568_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
-        animalConfig.entities.add(new Entity("Elephant", "eGI3RS52kJA","https://pmdvod.nationalgeographic.com/NG_Video/392/819/1142872131764_1516653097532_1142871619814_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
-        animalConfig.entities.add(new Entity("Horse", "5ocnVSh_ZF-","https://pmdvod.nationalgeographic.com/NG_Video/722/723/1251665987846_1528499941303_1251668547971_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
-        animalConfig.entities.add(new Entity("Pig", "bbPhEBl5Bh0","https://pmdvod.nationalgeographic.com/NG_Video/336/479/1152476739923_1517604362758_1152482371523_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4",new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
-        animalConfig.entities.add(new Entity("Wolf", "46bXrRt8pFF", "https://pmdvod.nationalgeographic.com/NG_Video/508/71/1453304387732_1551912110015_1453312067615_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
-        animalConfig.entities.add(new Entity("Tiger", "54KLm0HdFWy","https://pmdvod.nationalgeographic.com/NG_Video/854/911/Ohio_Tigers_wild_1800.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext()));
+        animalConfig.entities.add(new Entity("Lion", "3XAJojWxSWz","https://pmdvod.nationalgeographic.com/NG_Video/493/847/1087143491881_1509643865712_1087150659568_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
+        animalConfig.entities.add(new Entity("Elephant", "eGI3RS52kJA","https://pmdvod.nationalgeographic.com/NG_Video/392/819/1142872131764_1516653097532_1142871619814_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
+        animalConfig.entities.add(new Entity("Horse", "5ocnVSh_ZF-","https://pmdvod.nationalgeographic.com/NG_Video/722/723/1251665987846_1528499941303_1251668547971_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
+        animalConfig.entities.add(new Entity("Pig", "bbPhEBl5Bh0","https://pmdvod.nationalgeographic.com/NG_Video/336/479/1152476739923_1517604362758_1152482371523_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4",new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
+        animalConfig.entities.add(new Entity("Wolf", "46bXrRt8pFF", "https://pmdvod.nationalgeographic.com/NG_Video/508/71/1453304387732_1551912110015_1453312067615_mp4_video_1024x576_1632000_primary_audio_eng_3.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
+        animalConfig.entities.add(new Entity("Tiger", "54KLm0HdFWy","https://pmdvod.nationalgeographic.com/NG_Video/854/911/Ohio_Tigers_wild_1800.mp4", new Vector3(0.05f, 0.05f, 0.05f), AppConfig.getContext(), "animal"));
 
     }
 
